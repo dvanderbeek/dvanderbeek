e0ed6b24e692096769941d19aa0a735fc0bb9fec
David Van Der Beek <earlynovrock@gmail.com>
2013-11-21 08:18:07 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvanderbeek2::Application.config.secret_token = 'd35948a7d9ceae7dc302056a5daefe6f65f0c5ffc7daf32a6f8169b9c96a76e75206be0270f19f48d4f13d854ac42b8f21fec7de4f9e7781783f5817646513d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dvanderbeek2::Application.config.session_store :cookie_store, key: '_dvanderbeek2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dvanderbeek2::Application.config.session_store :active_record_store
